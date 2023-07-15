061a7e5c6c2fab036838a2132dcf7252d86f1b59
Hiromu-Watanabe <hiro_mu0166@icloud.com>
2023-07-15 18:55:58 +0900

    update: greetings.go summary: テスト失敗するパターンのコード追加

--- app/greetings/greetings.go
+++ app/greetings/greetings.go
@@ -14,7 +14,8 @@ func Hello(name string) (string, error) {
 	}
 
 	// Create a message using a random format.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)  // テスト成功パターン
+	message := fmt.Sprint(randomFormat()) // テスト失敗させるパターン
 	return message, nil
 }
 
